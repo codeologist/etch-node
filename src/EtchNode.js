d7b741f135c383821ba4361f5245ca0bdc0fb942
codeology <coder451@codeology.co.nz>
2015-11-10 09:35:50 +1300

    broken commit

--- src/EtchNode.js
+++ src/EtchNode.js
@@ -1,33 +1,76 @@
 
+    "use strict";
 
-    function EtchNode() {
-
+    function EtchNode( observerCallback ) {
+        this.eventListeners = [];
+        Object.observe( this, this.triggerEvent.bind( this, "change", null ) );
     }
 
-    EtchNode.extend = function(){
-
-        var constructors = Array.prototype.slice.call( arguments, 0 );
-
-        function ext(){
-            var args = arguments;
-            constructors.forEach( function( constructor ){
-                constructor.apply( this, args );
-            }, this);
-        }
-
-        function FauxNode(){
-            EtchNode.apply( this, arguments );
-            ext.apply( this, arguments );
-        }
-
-        constructors.forEach( function( constructor ){
-            Object.keys( constructor.prototype ).forEach( function( meth ){
-                FauxNode.prototype[meth]= constructor.prototype[meth];
+    EtchNode.prototype = {
+        createEventObject: function(){
+            return {};
+        },
+        addEventListener:function( repeat, type, callback ){
+            this.eventListeners.push({
+                type:type,
+                repeat:repeat,
+                callback:callback
             });
-        }, this);
+        },
+        removeEventListener:function( type, callback ){
+            this.eventListeners.forEach( function( event, i ){
+                if ( typeof callback === "function" && event.callback === callback && event.type === type ){
+                    delete this.eventListeners[i];
+                }
 
+                if ( callback === undefined && event.type === type ){
+                    delete this.eventListeners[i];
+                }
 
-        return FauxNode;
+                if ( type === undefined ){
+                    delete this.eventListeners[i];
+                }
+            }, this );
+        },
+        triggerEvent:function( type, eventObject ){
+            this.eventListeners.forEach( function( event ){
+                if ( event.repeat && event.type === type ){
+                    process.nextTick( event.callback.bind( this, eventObject ) );
+                    event.repeat--;
+                }
+            }, this );
+        }
     };
+
+    Object.defineProperty(EtchNode, "extend", {
+
+        value:function(){
+
+            var constructors = Array.prototype.slice.call( arguments, 0 );
+
+            function ext(){
+                var args = arguments;
+                constructors.forEach( function( constructor ){
+                    constructor.apply( this, args );
+                }, this);
+            }
+
+            function FauxNode(){
+                EtchNode.apply( this, arguments );
+                ext.apply( this, arguments );
+            }
+
+            FauxNode.prototype = EtchNode.prototype;
+
+            constructors.forEach( function( constructor ){
+                Object.keys( constructor.prototype ).forEach( function( meth ){
+                    FauxNode.prototype[meth]= constructor.prototype[meth];
+                });
+            }, this);
+
+            return FauxNode;
+        }
+    });
+
 
     module.exports = EtchNode;
--- test/EtchNode-test.js
+++ test/EtchNode-test.js
@@ -25,7 +25,7 @@
                 return 2;
             };
 
-            var ExtendedNode = EtchNode.extend( Extendable,Extendable2 );
+            var ExtendedNode = EtchNode.extend( Extendable, Extendable2 );
 
             assert( typeof ExtendedNode === "function");
             var extended = new ExtendedNode();
@@ -36,6 +36,148 @@
             assert.equal( extended.meth2(), 2);
 
             done();
+        });
+
+        it('should have basic event system', function(done){
+
+            var node = new EtchNode();
+
+            node.addEventListener( 1, "custom", function(){
+                done();
+            });
+
+            node.triggerEvent("custom");
+
+        });
+
+
+        it('should only trigger the requested event', function(done){
+
+            var node = new EtchNode();
+
+            node.addEventListener( 1, "custom", function(){
+                assert( false );
+            });
+            node.addEventListener( 1, "custom2", function(){
+                done();
+            });
+
+            node.triggerEvent("custom2");
+
+        });
+
+
+        it('should only trigger an event the repeat number of times', function(done){
+
+            var node = new EtchNode();
+
+            var count = 0;
+            node.addEventListener( 1, "custom", function(){
+                count++;
+            });
+
+            node.triggerEvent("custom");
+            node.triggerEvent("custom");
+
+            setTimeout( function(){
+                assert.equal( count, 1 );
+                done();
+            }, 0 );
+        });
+
+        it('should trigger the same event multiple times if it is added multiple times', function(done){
+
+            var node = new EtchNode();
+
+            var count = 0;
+            node.addEventListener( 1, "custom", function(){
+                count++;
+            });
+            node.addEventListener( 1, "custom", function(){
+                count++;
+            });
+
+            node.triggerEvent("custom");
+
+            setTimeout( function(){
+                assert.equal( count, 2 );
+                done();
+            }, 0 );
+        });
+
+        it('should remove an individual event', function(done){
+
+            var node = new EtchNode();
+            var func = function(){
+                assert(false);
+            };
+
+            node.addEventListener( 1, "custom", func);
+            node.addEventListener( 1, "custom", function(){
+                done();
+            });
+
+            node.removeEventListener( "custom", func );
+            node.triggerEvent("custom");
+
+        });
+
+        it('should remove a group of events', function(done){
+
+            var node = new EtchNode();
+
+            node.addEventListener( 1, "custom",  function(){
+                assert(false);
+            });
+
+            node.addEventListener( 1, "custom", function(){
+                assert(false);
+            });
+
+            node.removeEventListener( "custom" );
+            node.triggerEvent("custom");
+
+            setTimeout( function(){
+                done();
+            }, 0 );
+        });
+
+        it('should remove all events', function(done){
+
+            var node = new EtchNode();
+
+            node.addEventListener( 1, "custom",  function(){
+                assert(false);
+            });
+
+            node.addEventListener( 1, "custom",  function(){
+                assert(false);
+            });
+
+            node.addEventListener( 1, "custom2", function(){
+                assert(false);
+            });
+
+            node.removeEventListener();
+            node.triggerEvent("custom");
+            node.triggerEvent("custom2");
+
+            setTimeout( function(){
+                done();
+            }, 0 );
+        });
+
+        it("shouldn't remove default events", function(done){
+
+            var node = new EtchNode();
+
+            node.addEventListener( 1, "default",  function(){
+                done();
+            }, true);
+
+            node.removeEventListener();
+            node.triggerEvent("default");
+
         });
     });
 
